688a58e1385df59a287538b5720522d8e9b1637f
idop <ido.perry@gmail.com>
2015-04-04 18:33:54 +0300

    adde shit

---
+++ CPPProject/CPPProject/Player.h
@@ -0,0 +1,57 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Player.h
+// -------------
+// This class creates and handles the players of the game
+// This class will  create the player , draw the player , move the player , etc..
+//
+// Author: Ido Perry
+// First version: 2015-04-03
+//
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#pragma once
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+#include "Point.h"
+#include "ScreenObject.h"
+#include <string>
+#include "Direction.h"
+using namespace std;
+
+
+const char  P1_DEFULT_CHAR='@';
+const char  P2_DEFULT_CHAR='#';
+const string  P1_KEYBOARD_KEYS = "awdx";
+const string  P2_KEYBOARD_KEYS = "jilm";
+const Point P1_DEFULT_POSITION(10, 9);
+const Point P2_DEFULT_POSITION(70, 9);
+const Direction::value P1_DEFULT_DIRECTION = Direction::RIGHT;
+const Direction::value P2_DEFULT_DIRECTION = Direction::LEFT;
+
+
+class Player : public ScreenObject
+{
+	const char playerChar;
+	const string keyboardKeys;
+	int numberOfLives;
+	int score;
+	Point position;
+	Direction::value direction;
+public:
+	Player(const char & ch, const string & str) :playerChar(ch), keyboardKeys(str),score(0) {}; // constract the play with the defult attributes (player char , keyboard commands and starting score
+	void SetToStart(const Point & p, const Direction::value & d); // retrun the player to deuflt start level settings
+	void SetDirection(const Direction::value & d){ direction = d; };
+	void SetNumberOfLives(const int & num){ numberOfLives = num; };
+	int getNumberOfLives()const { return numberOfLives; };
+	int getScore()const { return score; };
+	Direction::value getdirection()const { return direction; };
+	void Move();
+	virtual char Type()const{ return 'p'; }
+	virtual void Draw()const;
+	virtual void Clear()const;
+	virtual Point GetPosition()const{ return position; }
+	virtual unsigned int GetData()const { return score; } // we dont really need this here
+};
+
+
+#endif
---
+++ CPPProject/CPPProject/Point.h
@@ -0,0 +1,25 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Point.h
+// -------------
+// simple class to store screen points
+//
+// Author: Ido Perry
+// First version: 2015-03-29
+//
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#ifndef _POINT_H_
+#define _POINT_H_
+
+
+class Point
+{
+	int x, y;
+public:
+	Point(){}
+	Point(int x1, int y1) :x(x1), y(y1){}
+	int getX()const{ return x;}
+	int getY()const{ return y;}
+	void setX(int x1){ x = x1; }
+	void setY(int y1){ y = y1; }
+};
+#endif
