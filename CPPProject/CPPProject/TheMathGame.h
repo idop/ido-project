ca73d059cf01f2e0b9dad87e26490edf51b35ab3
idop <ido.perry@gmail.com>
2015-04-12 21:51:20 +0300

    added comments

--- CPPProject/CPPProject/TheMathGame.h
+++ CPPProject/CPPProject/TheMathGame.h
@@ -29,26 +29,27 @@ class TheMathGame : public ISpecificGame
 	unsigned int maxLevel;
 	unsigned int currentTurn;
 	bool levelDone;
-	void PrintScores();
-	void EndTurn(); 
-	void keyStrokeManager(const list<char>& keyHits);
-	void PlayerMovment(const Point & toMove, Player & p, Equation & eq);
-	void clearAndMove(Player & p, const Point & toMove, ScreenObject * obj);
-	void CheckSolution(Equation eq, const ScreenObject * obj, Player & p);
-	void DrawEquations()const;
-	Point GetPointToMove(const Player & p);
 	Equation equation1, equation2;
 	Player player1, player2;
 	Screen * currentScreen;
-	Direction::value MapKeyToDirection(const char & keyHit, const Player & p);
-	Color GetColorForText()const;
+
+	void PrintScores()const;// this function prints the game/level stats (scores, lives,current level and current turn), refreshes each time there is a change
+	void EndTurn(); //this function handles all end of turn events.
+	void keyStrokeManager(const list<char>& keyHits); //this function will handle the players keystorkes and change theier direction accordingly
+	void PlayerMovment(const Point & toMove, Player & p, Equation & eq);// this function manages the player movment
+	void clearAndMove(Player & p, const Point & toMove, ScreenObject * obj);//halper function to Player movment
+	void CheckSolution(Equation eq, const ScreenObject * obj, Player & p);//checks if the player found a valid solution or not and acts accordingly
+	void DrawEquations()const;//draws the 2 equations to the screen
+	Point GetPointToMove(const Player & p);//get the point the player needs to move to according to his direction
+	Direction::value MapKeyToDirection(const char & keyHit, const Player & p);//helper function to keyStrokeManager
+	Color GetColorForText()const; //this function will retrun a color for the screen text based on the current level. each level we will replace the screen color
 
 public:
 
 	TheMathGame() : currentLevel(0), maxLevel(MAX_LEVEL), currentTurn(0), levelDone(false), equation1(P1_EQUATION_POSITION), equation2(P2_EQUATION_POSITION),
-		player1(P1_DEFULT_CHAR, P1_KEYBOARD_KEYS), player2(P2_DEFULT_CHAR, P2_KEYBOARD_KEYS){currentScreen=NULL;};
+		player1(P1_DEFULT_CHAR, P1_KEYBOARD_KEYS), player2(P2_DEFULT_CHAR, P2_KEYBOARD_KEYS){currentScreen=NULL;}
 	virtual bool isLevelDone()const{ return levelDone; } 
-	virtual bool hasNextLevel()const{ return (currentLevel < maxLevel); };
+	virtual bool hasNextLevel()const{ return (currentLevel < maxLevel); }
 	virtual void startLevel(unsigned int level);
 	virtual void doIteration(const list<char>& keyHits);
 	virtual void doSubIteration();
